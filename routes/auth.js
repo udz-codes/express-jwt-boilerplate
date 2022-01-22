c8a1d99dd4a1baa082955b60f5b9a992ea363a72
Ujjwaldeep <ujjwal2699singh@gmail.com>
2022-01-22 16:59:56 +0530

    Status code fix

--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,11 @@ router.post('/register', async (req, res) => {
 
     // Validation check
     const {error} = registerValidation(req.body);
-    if(error) return res.send(error.details[0].message).status(400);
+    if(error) return res.status(400).send(error.details[0].message);
 
     // Email uniqueness check
     const emailExists = await User.findOne({email: req.body.email});
-    if(emailExists) return res.send('Email address already exists').status(400);
+    if(emailExists) return res.status(400).send('Email address already exists');
 
     // Hash the password
     const salt = bcrypt.genSaltSync(10);
@@ -38,15 +38,15 @@ router.post('/login', async (req, res) => {
 
     // Validation check
     const {error} = loginValidation(req.body);
-    if(error) return res.send(error.details[0].message).status(400);
+    if(error) return res.status(400).send(error.details[0].message);
 
     // Email existance check
     const registeredUser = await User.findOne({email: req.body.email});
-    if(!registeredUser) return res.send('User with this email does not exist').status(400);
+    if(!registeredUser) return res.status(400).send('User with this email does not exist');
 
     // Check password
     const passwordMatch = bcrypt.compareSync(req.body.password, registeredUser.password);
-    if(!passwordMatch) return res.send('Email or Password do not match').status(400);
+    if(!passwordMatch) return res.status(400).send('Email or Password do not match');
 
     // Create and assign JWT
     const token = jwt.sign({_id: registeredUser._id}, process.env.JWT_SECRET);
